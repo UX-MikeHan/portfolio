dc320fa981e94277317bd454e057874222146d8e
UX-MikeHan <40835718+UX-MikeHan@users.noreply.github.com>
2018-07-24 20:41:04 +0200

    Add files via upload
    testing

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hey, welcome to my world. A world of trial and error. But this is what defines me!")
+console.log("I was born in Cologne, Germany. I am a \"Kölsche Jung\"")
+var age = 40;
+var name = "Mike";
+var height = 1.71;
+console.log(age);
+console.log(name);
+console.log(height);
+var text1 = "My parents are orginally from Seoul, South Korea.";
+var text2 = "They moved to germany in the year 1968.";
+console.log (text1,text2);
